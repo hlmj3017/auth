39f78ed4854c89039bcb29ca3d810ba1c90cfdd0
hlmj3 <hlmj3017@naver.com>
2023-08-29 10:28:21 +0900

    article Index 기능 구현

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 
 
 def index(request):
-    return render()
+
+    articles = Article.objects.all()  # 전체 게시물 조회
+
+    context = {
+        'articles':articles,          # context에 전체 게시물 담고
+    }
+
+    return render(request, 'index.html', context)
